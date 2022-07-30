82d54b0e8681148382ff127f6c4da8817a7cb422
QunFan <allenfan1012@gmail.com>
2022-07-30 10:21:59 +1000

    commit tree 6c516f526b59dd12d01cdd3b2f23ebfc1a210aa7

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31;
+let multiply = num1 * num2;
+ console.log (multiply);
+
+ let random = Math.ceil (Math.random()*10);
+console.log (random);
+
+const num3 = 40
+const num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max (10,20);
+console.log (max);
